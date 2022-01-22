2cb9eeef17cd68e048f0c2f2ac418e13325880b4
Suliman Sattar <suliman.sattar@kcl.ac.uk>
2022-01-22 23:51:56 +0000

    Add Sign Up View

---
+++ clubs/views.py
@@ -0,0 +1,30 @@
+"""Views of the microblogs app."""
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseForbidden
+from django.shortcuts import redirect, render
+from .forms import SignUpForm
+from .models import User
+from .helpers import login_prohibited
+
+@login_prohibited
+def home(request):
+    return render(request, 'home.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+@login_prohibited
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})
