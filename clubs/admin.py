bc25c72cbd5a93a1e3df9f247644728b5a6f6c13
Suliman Sattar <suliman.sattar@kcl.ac.uk>
2022-01-22 23:49:31 +0000

    Add Admin Interface

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,3 +1,12 @@
+"""Configuration of the admin interface for clubs."""
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for users."""
+
+    list_display = [
+        'id', 'username', 'first_name', 'last_name', 'email', 'is_active',
+    ]
