ab942338dced0883d8bb982049613f10d4483015
NithilaDaby <nithila.daby@kcl.ac.uk>
2022-01-23 12:24:50 +0000

    Refactor views.py into one file

---
+++ clubs/views/static_views.py
@@ -0,0 +1,7 @@
+"""Static views of the clubs app."""
+from django.shortcuts import render
+from clubs.helpers import login_prohibited
+
+@login_prohibited
+def home(request):
+    return render(request, 'home.html')
