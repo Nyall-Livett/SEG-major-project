b545c9e09c645ec25b51f15669cede9951bf9ab2
Prabin Gurung <prabin.gurung@kcl.ac.uk>
2022-01-27 10:28:00 +0000

    make all models appear in admin
    -make clubs each user is member of appear for user
    -make each user that is member of a club appear in club
    -make meeting interface in admin

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,12 +1,30 @@
 """Configuration of the admin interface for clubs."""
 from django.contrib import admin
-from .models import User
+from .models import User, Club, Meeting
 
+class MembershipInline(admin.TabularInline):
+    model = Club.members.through
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for users."""
 
-    list_display = [
-        'id', 'username', 'first_name', 'last_name', 'email', 'is_active',
+    inlines = [
+        MembershipInline,
+        ]
+    exclude = ('password',)
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for Club"""
+    
+    inlines = [
+        MembershipInline,
     ]
+    exclude = ('members',)
+
+@admin.register(Meeting)
+class MeetingAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for Meeting"""
+
+    list_display = ['date', 'club', 'URL' ,'member_selected', 'next_book' ,'meeting_notes',]
