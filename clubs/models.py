3609b0ba5dfeadc7f9233423514fb747802de298
Nyall Livett <nyall.livett@kcl.ac.uk>
2022-01-28 17:53:51 +0000

    Add maximum_members to club model

--- clubs/models.py
+++ clubs/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from libgravatar import Gravatar
 from django.utils import timezone
 from datetime import date, datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
 import pytz
 
 """used for meeting model"""
@@ -45,6 +46,7 @@ class Club(models.Model):
     leader = models.ForeignKey(User, related_name="leader_of", on_delete=models.PROTECT)
     members = models.ManyToManyField(User, symmetrical=True, related_name="clubs")
     theme = models.CharField(max_length=512, blank=False)
+    maximum_members = models.IntegerField(blank=False, default=2, validators=[MinValueValidator(1), MinValueValidator(64)])
 
     def add_member(self, user):
         if user not in self.members.all():
@@ -54,30 +56,30 @@ class Club(models.Model):
         self.leader = user
         self.save()
 
+    def member_count(self):
+        return f"{self.members.count()}/{self.maximum_members}"
+
     def __str__(self):
         return self.name
-    
+
 
 class Book(models.Model):
     """Book model"""
     name = models.CharField(max_length=64, unique=True, blank=False)
     description = models.CharField(max_length=2048, blank=False)
-    
+
 class Meeting(models.Model):
     """Meeting model"""
     date = models.DateTimeField("date", default=timezone.now)
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
     members = models.ManyToManyField(User, related_name="members")
     book = models.ManyToManyField(Book, related_name="book")
-    
-    
+
+
     def future_meetings(self):
         utc=pytz.UTC
-        list = [] 
+        list = []
         for i in Meeting.objects.all():
             if i.date.replace(tzinfo=utc) > datetime.now().replace(tzinfo=utc):
                 list.append(i)
         return list
-
-    
-
