f7a9d5787b52de033257d0cb89a5f16308b0d718
NyallLivettKingsCollege <nyall.livett@kcl.ac.uk>
2022-02-10 19:15:46 +0000

    Add more tests for DeleteNotification view

--- clubs/tests/views/test_delete_notification_view.py
+++ clubs/tests/views/test_delete_notification_view.py
@@ -11,23 +11,33 @@ class DeleteNotificationTestCase(TestCase, LogInTester):
         'clubs/tests/fixtures/default_user.json',
         'clubs/tests/fixtures/other_users.json',
         'clubs/tests/fixtures/default_notification.json',
+        'clubs/tests/fixtures/other_notifications.json'
     ]
 
     def setUp(self):
         self.url = reverse('notification_delete')
         self.default_user = User.objects.get(pk = 1)
 
-
-    # Test URL is correct
     def test_log_in_url(self):
         self.assertEqual(self.url,'/notification_delete/')
 
-    # Test get access case for users
     def test_get_access_for_unauthenticated(self):
         self.assertFalse(self._is_logged_in())
         response = self.client.post(self.url, follow=True)
         response_url = reverse_with_next('log_in', self.url)
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+
+    def test_get_access_for_authenticated_throws_405(self):
+        self.client.login(username=self.default_user, password='Password123')
+        self.assertTrue(self._is_logged_in())
+        response = self.client.get(self.url, {'notifications': '["1"]'}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+        self.assertEqual(response.status_code, 405)
+
+    def test_returns_status_code_200(self):
+        self.client.login(username=self.default_user, password='Password123')
+        self.assertTrue(self._is_logged_in())
+        response = self.client.post(self.url, {'notifications': '["1"]'}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+        self.assertEqual(response.status_code, 200)
 
     def test_notification_gets_deleted(self):
         self.client.login(username=self.default_user, password='Password123')
@@ -36,3 +46,11 @@ class DeleteNotificationTestCase(TestCase, LogInTester):
         response = self.client.post(self.url, {'notifications': '["1"]'}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
         count_after = Notification.objects.count()
         self.assertEqual(count_before -1, count_after)
+
+    def test_multiple_notifications_gets_deleted_at_once(self):
+        self.client.login(username=self.default_user, password='Password123')
+        self.assertTrue(self._is_logged_in())
+        count_before = Notification.objects.count()
+        response = self.client.post(self.url, {'notifications': '["1","2"]'}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+        count_after = Notification.objects.count()
+        self.assertEqual(count_before -2, count_after)
