b84b5806450027d638f6a7ab61b5fbdfb543d43d
Suliman Sattar <suliman.sattar@kcl.ac.uk>
2022-02-14 00:21:56 +0000

    Update Dashboard

--- clubs/views/dashboard_views.py
+++ clubs/views/dashboard_views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from clubs.models import Book, Club
+import random
+
+book_list = list(Book.objects.all())
+if (Book.objects.count() >= 3):
+    books = random.sample(book_list, 3)
+elif(Book.objects.count() >= 2):
+    books = random.sample(book_list, 2)
+elif(Book.objects.count() >= 1):
+    books = random.sample(book_list, 1)
+else:
+    books = random.sample(book_list, 0)
+
+club_list = list(Club.objects.all())
+if (Club.objects.count() >= 3):
+    clubs = random.sample(club_list, 3)
+elif(Club.objects.count() >= 2):
+    clubs = random.sample(club_list, 2)
+elif(Club.objects.count() >= 1):
+    clubs = random.sample(club_list, 1)
+else:
+    clubs = random.sample(club_list, 0)
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard.html', {'books': books, 'clubs': clubs})
 
 
 
@@ -11,5 +33,5 @@ def dashboard(request):
 #     """Render log in template with blank log in form."""
 #     def render(self):
 #         self.request.
-        
+
 #         return render(self.request, 'log_in.html', {'form': form, 'next': self.next})
