3e196360485c6c09b793514a089de07e11cf9be8
NyallLivettKingsCollege <nyall.livett@kcl.ac.uk>
2022-02-16 18:00:01 +0000

    Add Create Moment view

---
+++ clubs/views/moment_views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from clubs.enums import MomentType
+from clubs.models import Moment
+from django.views import View
+from django.http import JsonResponse
+import json
+
+
+class CreateMomentView(LoginRequiredMixin, View):
+    """docstring for CreateMomentView."""
+
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            moment_body = json.loads(request.POST.get('moment_body'))
+            new_moment = Moment(
+                body=moment_body,
+                type=MomentType.CUSTOM,
+                user=request.user
+            )
+
+            try:
+                new_moment.full_clean()
+                new_moment.save()
+                return JsonResponse({}, status=200)
+            except ValidationError as e:
+                return JsonResponse({}, status=200)
+        raise PermissionDenied()
