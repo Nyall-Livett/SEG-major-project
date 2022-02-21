84347549fb2104e7b183940be521d236a8b76a91
NyallLivettKingsCollege <nyall.livett@kcl.ac.uk>
2022-02-21 18:48:49 +0000

    Set up random clubs for dashboard view

--- clubs/views/dashboard_views.py
+++ clubs/views/dashboard_views.py
@@ -12,10 +12,14 @@ class DashboardView(LoginRequiredMixin, ListView):
     template_name = 'dashboard.html'
 
     def get_queryset(self):
-        user_moments = self.request.user.moment_set.all()
+        user_moments = self.request.user.moment_set.all().order_by('-created_on')
         return user_moments
 
     def get_context_data(self, **kwargs):
+        club_list = list(Club.objects.all())
+        clubs_range = range(len(club_list))
+        self.clubs_sample = list(random.sample(club_list, len(club_list) if len(club_list) < 7 else 6))
         context = super().get_context_data(**kwargs)
         context['form'] = MomentForm()
+        context['random_clubs'] = self.clubs_sample
         return context
