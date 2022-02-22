07207d40432f448e44aabef0e0356f8daca01193
NyallLivettKingsCollege <nyall.livett@kcl.ac.uk>
2022-02-22 20:51:45 +0000

    Reduced size of mini gravatar for use model

--- clubs/models.py
+++ clubs/models.py
@@ -41,7 +41,7 @@ class User(AbstractUser):
 
     def mini_gravatar(self):
         """Return a URL to a miniature version of the user's gravatar."""
-        return self.gravatar(size=60)
+        return self.gravatar(size=50)
 
     def future_meetings(self):
         utc=pytz.UTC
