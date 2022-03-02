b3e48a4362333f4fcd8b474be5f772e06fec767f
NyallLivettKingsCollege <nyall.livett@kcl.ac.uk>
2022-03-02 11:25:16 +0000

    Add avatar icon and color enums

--- clubs/enums.py
+++ clubs/enums.py
@@ -14,3 +14,26 @@ class MomentType(models.IntegerChoices):
     CLUB_CREATED = 2
     BOOK_RECOMMENDATION = 3
     READING_NEW_BOOK = 4
+
+class AvatarIcon(models.IntegerChoices):
+    BRUTUS = 0, "brutus"
+    BILL = 1, "bill"
+    GENIE = 2, "genie"
+    GRINCH = 3, "grinch"
+    JERRY = 4, "jerry"
+    KEIJI = 5, "keiji"
+    KERMIT = 6, "kermit"
+    KUROO = 7, "kuroo"
+    LION = 8, "lion"
+    MELODY = 9, "melody"
+    MERMAID = 10, "mermaid"
+    MORTY = 11, "morty"
+    TURTLE = 12, "turtle"
+    CHEBURASHKA = 13, "cheburashka"
+    AMETHYST = 14, "amethyst"
+
+class AvatarColor(models.IntegerChoices):
+    BLUE = 0, "#70bbfb"
+    RED = 1, "#ff7d7d"
+    YELLOW = 2, "#f1fc5a"
+    GREEN = 3, "#55d634"
