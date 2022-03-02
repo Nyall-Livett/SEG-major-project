e22ba9401021e7f5ab20786348d4b53a96b4071f
Prabin Gurung <k20064822@kcl.ac.uk>
2022-03-02 17:01:59 +0000

    Make meeting URL load when clicking on join

--- clubs/zoom_api_url_generator_helper.py
+++ clubs/zoom_api_url_generator_helper.py
@@ -43,9 +43,9 @@ def create_JSON_meeting_data(meetingTitle, startTime, meetingDescription):
 # function that returns a zoom meeting URL
 def getZoomMeetingURL(meetingDetails):
     headers = {'authorization' : 'Bearer ' + createjwt(), 'content-type': 'application/json'}
-    r = requests.post(f'https://api.zoom.us/v2/users/me/meetings', headers = headers, data = json.dumps(meetingDetails))
-    y = json.loads(r.text)
-    return y["join_url"]
+    response = requests.post(f'https://api.zoom.us/v2/users/me/meetings', headers = headers, data = json.dumps(meetingDetails))
+    json_response = json.loads(response.text)
+    return json_response["join_url"]
 
 
 # function to convert datetime object to json serializable string accepted as time by zoom API
