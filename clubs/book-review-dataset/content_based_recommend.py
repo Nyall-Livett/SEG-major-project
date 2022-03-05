1331d079d2cffc6dee0abb080210ba47233d3530
Asif Alam <alamtheboss232@gmail.com>
2022-03-05 12:14:41 +0000

    Add content based recommender method

--- clubs/book-review-dataset/content_based_recommend.py
+++ clubs/book-review-dataset/content_based_recommend.py
@@ -14,8 +14,8 @@ books.rename(columns = {'ISBN':'isbn', 'Book-Title':'book_title', 'Book-Author':
 users.rename(columns = {'User-ID':'user_id', 'Location':'location', 'Age':'age'}, inplace=True)
 ratings.rename(columns = {'User-ID':'user_id', 'ISBN':'isbn', 'Book-Rating':'rating'}, inplace=True)
 
-print(list(books.columns))
-print(books)
+# print(list(books.columns))
+# print(books)
 # print(list(users.columns))
 # print(users)
 # print(list(ratings.columns))
@@ -36,61 +36,60 @@ df.drop(columns = ['location','isbn', 'img_s','img_m','age',],axis=1,inplace = T
 
 df.drop(index=df[df['rating'] == 0].index, inplace=True)
 
-print(list(df.columns))
-print (df)
+# print(list(df.columns))
+# print (df)
 
 
 
-# def content_based_recommender(book_title):
+def content_based_recommender(book_title):
     
-#     book_title = str(book_title)
-#     if book_title in df['book_title'].values:
-#         rating_counts = pd.DataFrame(df['book_title'].value_counts())
-#         rare_books = rating_counts[rating_counts['book_title'] <= 50].index
-#         common_books = df[~df['book_title'].isin(rare_books)]
+    book_title = str(book_title)
+    if book_title in df['book_title'].values:
+        rating_counts = pd.DataFrame(df['book_title'].value_counts())
+        rare_books = rating_counts[rating_counts['book_title'] <= 100].index
+        common_books = df[~df['book_title'].isin(rare_books)]
         
-#         if book_title in rare_books:
+        if book_title in rare_books:
             
-#             random = pd.Series(common_books['book_title'].unique()).sample(5).values
-#             print('There are no recommendations for this book')
-#             print('Try: \n')
-#             print('{}'.format(random[0]),'\n')
-#             print('{}'.format(random[1]),'\n')
-#             print('{}'.format(random[2]),'\n')
-#             print('{}'.format(random[3]),'\n')
-#             print('{}'.format(random[4]),'\n')
-#         else:
+            random = pd.Series(common_books['book_title'].unique()).sample(5).values
+            print('There are no recommendations for this book')
+            print('Try: \n')
+            print('{}'.format(random[0]),'\n')
+            print('{}'.format(random[1]),'\n')
+            print('{}'.format(random[2]),'\n')
+            print('{}'.format(random[3]),'\n')
+            print('{}'.format(random[4]),'\n')
+        else:
             
-#             common_books = common_books.drop_duplicates(subset=['book_title'])
-#             common_books.reset_index(inplace= True)
-#             common_books['index'] = [i for i in range(common_books.shape[0])]
-#             target_cols = ['book_title','book_author','publisher', 'Category']
-#             common_books['combined_features'] = [' '.join(common_books[target_cols].iloc[i,].values) for i in range(common_books[target_cols].shape[0])]
-#             cv = CountVectorizer()
-#             count_matrix = cv.fit_transform(common_books['combined_features'])
-#             cosine_sim = cosine_similarity(count_matrix)
-#             index = common_books[common_books['book_title'] == book_title]['index'].values[0]
-#             sim_books = list(enumerate(cosine_sim[index]))
-#             sorted_sim_books = sorted(sim_books,key=lambda x:x[1],
-#                                       reverse=True)[1:6]
+            common_books = common_books.drop_duplicates(subset=['book_title'])
+            common_books.reset_index(inplace= True)
+            common_books['index'] = [i for i in range(common_books.shape[0])]
+            target_cols = ['book_title','book_author','publisher']
+            common_books['combined_features'] = [' '.join(common_books[target_cols].iloc[i,].values) for i in range(common_books[target_cols].shape[0])]
+            cv = CountVectorizer()
+            count_matrix = cv.fit_transform(common_books['combined_features'])
+            cosine_sim = cosine_similarity(count_matrix)
+            index = common_books[common_books['book_title'] == book_title]['index'].values[0]
+            sim_books = list(enumerate(cosine_sim[index]))
+            sorted_sim_books = sorted(sim_books,key=lambda x:x[1],
+                                      reverse=True)[1:6]
             
-#             books = []
-#             for i in range(len(sorted_sim_books)):
-#                 books.append(common_books[common_books['index'] == sorted_sim_books[i][0]]['book_title'].item())
-            
-#             return books
+            books = []
+            for i in range(len(sorted_sim_books)):
+                books.append(common_books[common_books['index'] == sorted_sim_books[i][0]]['book_title'].item())
+            return books
 
-#     else:
-#       print('Cant find book in dataset, please check spelling')
+    else:
+      print('Cant find book in dataset, please check spelling')
 
-# book1 = content_based_recommender("Husband, Lover, Stranger (Husband, Lover, Stranger)")
-# print(book1)
-# book2 = content_based_recommender("The Testament")
-# print(book2)
-# book3 = content_based_recommender("1st to Die: A Novel")
-# print(book3)
-# book4 = content_based_recommender("Harry Potter and the Order of the Phoenix (Book 5)")
-# print(book4)
-# book5 = content_based_recommender("Fahrenheit 451")
-# print(book5)
+book1 = content_based_recommender("Husband, Lover, Stranger (Husband, Lover, Stranger)")
+print(book1)
+book2 = content_based_recommender("The Testament")
+print(book2)
+book3 = content_based_recommender("1st to Die: A Novel")
+print(book3)
+book4 = content_based_recommender("Harry Potter and the Order of the Phoenix (Book 5)")
+print(book4)
+book5 = content_based_recommender("Fahrenheit 451")
+print(book5)
 
