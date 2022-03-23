d104dbfe0cfe6c89a2a3c0727e3080c1f7d0e98a
Bardia Rokhzadifar <k2036646@kcl.ac.uk>
2022-03-23 22:26:36 +0000

    complete test cases for test set_book

--- clubs/tests/forms/test_set_book_form.py
+++ clubs/tests/forms/test_set_book_form.py
@@ -40,6 +40,13 @@ class SetBookTestCase(TestCase ,LogInTester):
         response = self.client.get(self.url, follow=True)
         self.assertTemplateUsed(response, 'set_book.html')
 
+    
+    def test_get_access_for_unauthenticated(self):
+        self.assertFalse(self._is_logged_in())
+        response = self.client.get(self.url, follow=True)
+        response_url = reverse_with_next('log_in', self.url)
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+
     def test_set_book_redirects_when_user_not_logged_in(self):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.get(self.url)
@@ -61,5 +68,76 @@ class SetBookTestCase(TestCase ,LogInTester):
         form = BookForm(data=self.form_input)
         self.assertTrue(form.is_valid())
 
+    def test_isbn_must_not_be_blank(self):
+        self.form_input['isbn'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
 
+    def test_name_must_not_be_blank(self):
+        self.form_input['name'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_author_must_not_be_blank(self):
+        self.form_input['author'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_book_description_can_be_blank(self):
+        self.form_input['description'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_publication_year_can_be_blank(self):
+        self.form_input['publication_year'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_publisher_can_be_blank(self):
+        self.form_input['publisher'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_image_url_s_can_be_blank(self):
+        self.form_input['image_url_s'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_image_url_m_can_be_blank(self):
+        self.form_input['image_url_m'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_image_url_l_can_be_blank(self):
+        self.form_input['image_url_l'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_category_can_be_blank(self):
+        self.form_input['category'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_grouped_category_can_be_blank(self):
+        self.form_input['grouped_category'] = ''
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_must_save_correctly(self):
+        form = BookForm(data=self.form_input)
+        before_count = Book.objects.count()
+        form.save()
+        after_count = Book.objects.count()
+        self.assertEqual(after_count, before_count+1)
+        book = Book.objects.get(isbn='0195153431')
+        self.assertEqual(book.name, 'Holographic universe')
+        self.assertEqual(book.description, 'This book is about vision...')
+        self.assertEqual(book.author, 'janedoe')
+        self.assertEqual(book.publication_year, '2002')
+        self.assertEqual(book.publisher, 'London Publisher')
+        self.assertEqual(book.image_url_s, 'http://images.amazon.com/images/P/0195153448.01.THUMBZZZ.jpg')
+        self.assertEqual(book.image_url_m, 'http://images.amazon.com/images/P/0195153448.01.MZZZZZZZ.jpg')
+        self.assertEqual(book.image_url_l, 'http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg')
+       
+      
 
