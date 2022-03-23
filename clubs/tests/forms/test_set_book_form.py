5a0ee434a56d0bb4737cd0d34131fedfed3713e5
Bardia Rokhzadifar <k2036646@kcl.ac.uk>
2022-03-23 21:55:33 +0000

    add more test set_book

--- clubs/tests/forms/test_set_book_form.py
+++ clubs/tests/forms/test_set_book_form.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import messages
 from django.test import TestCase
 from django.urls import reverse
-from clubs.forms import BookReviewForm, MeetingForm
+from clubs.forms import BookForm, BookReviewForm, MeetingForm
 from clubs.models import Book, User, Club
 from clubs.tests.helpers import LogInTester, reverse_with_next
 
@@ -21,6 +21,19 @@ class SetBookTestCase(TestCase ,LogInTester):
         self.club = Club.objects.get(name='Oxford Book Club')
         self.book = Book.objects.get(isbn= "0195153448")
         self.url = reverse('book')
+        self.form_input = {
+            'isbn': '0195153431',
+            'name': 'Holographic universe',
+            'description': 'This book is about vision...',
+            'author': 'janedoe',
+            'publication_year': '2002',
+            'publisher': 'London Publisher',
+            'image_url_s': 'http://images.amazon.com/images/P/0195153448.01.THUMBZZZ.jpg',
+            'image_url_m': 'http://images.amazon.com/images/P/0195153448.01.MZZZZZZZ.jpg',
+            'image_url_l': 'http://images.amazon.com/images/P/0195153448.01.LZZZZZZZ.jpg',
+            'category': 'Humor',
+            'grouped_category': 'Humor',
+        }
        
     def test_set_book_uses_correct_template(self):
         self.client.login(username=self.user.username, password='Password123')
@@ -31,3 +44,22 @@ class SetBookTestCase(TestCase ,LogInTester):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_set_book_has_required_fields(self):
+        form = BookForm()
+        self.assertIn('name', form.fields)
+        self.assertIn('description', form.fields)
+        self.assertIn('isbn', form.fields)
+        self.assertIn('author', form.fields)
+        self.assertIn('publisher', form.fields)
+        self.assertIn('publication_year', form.fields)
+        self.assertIn('image_url_s', form.fields)
+        self.assertIn('image_url_m', form.fields)
+        self.assertIn('image_url_l', form.fields)
+
+    def test_valid_set_book_form(self):
+        form = BookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+
+
