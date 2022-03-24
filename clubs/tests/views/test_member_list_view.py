11fb402fdcd7d3168d342c8f7f5c519042408af4
Bardia Rokhzadifar <k2036646@kcl.ac.uk>
2022-03-24 14:52:21 +0000

    add member list test

---
+++ clubs/tests/views/test_member_list_view.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.test import TestCase
+from django.urls import reverse
+from clubs.models import Club, User
+from clubs.tests.helpers import LogInTester, reverse_with_next
+
+class MemberListTest(TestCase, LogInTester):
+
+    fixtures = ['clubs/tests/fixtures/default_user.json',
+                'clubs/tests/fixtures/default_club.json',
+                'clubs/tests/fixtures/other_users.json'
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(username='johndoe')
+        self.club = Club.objects.get(name='Oxford Book Club')
+        self.url = reverse('member_list' , kwargs={'club_id': self.club.id})
+
+    def test_member_list_url(self):
+        self.assertEqual(self.url,f'/member_list/{self.club.id}')
+
+    def test_correct_template_used(self):
+        self.client.login(username=self.user, password='Password123')
+        self.assertTrue(self._is_logged_in())
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'member_list.html')
+
+    
