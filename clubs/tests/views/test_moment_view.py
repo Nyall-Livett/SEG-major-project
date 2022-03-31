346ffa84f257c3499adf97abc748e863c23aea64
NyallLivettKingsCollege <nyall.livett@kcl.ac.uk>
2022-03-31 17:24:09 +0100

    Fix faulty test

--- clubs/tests/views/test_moment_view.py
+++ clubs/tests/views/test_moment_view.py
@@ -19,23 +19,16 @@ class MomentTestCase(TestCase, LogInTester):
         self.club = Club.objects.get(pk=1)
         self.book = Club.objects.get(pk=1)
         self.url = reverse('create_moment')
-        self.data = {'body': 'sdfsdfsdfs'}
 
     def test_moment_created_correctly(self):
         self.client.login(username=self.user.username, password='Password123')
         num = Moment.objects.all().count()
-        self.assertEqual(num,0)
-        new_moment = Moment.objects.create(body = 'New moment', type = MomentType.CUSTOM, user = self.user) 
-        new_moment.full_clean()
-        new_moment.save()
+        self.assertEqual(num, 0)
+        response = self.client.post(self.url, {'moment_body': '"This is a moment"'}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+        new_moment = Moment.objects.first()
         num_2 = Moment.objects.all().count()
         self.assertEqual(num_2,1)
         self.assertEqual(new_moment.type,MomentType.CUSTOM)
         self.assertEqual(new_moment.type, 0)
         saved_moment = Moment.objects.all()[0]
         self.assertEqual(saved_moment, new_moment)
-        
-        
-        
-
-    
