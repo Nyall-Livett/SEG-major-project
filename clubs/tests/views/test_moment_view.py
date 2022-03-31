3302737482fa93fc934eb9f9bd1918bb56832082
Bardia Rokhzadifar <k2036646@kcl.ac.uk>
2022-03-31 13:47:46 +0100

    test moment view

---
+++ clubs/tests/views/test_moment_view.py
@@ -0,0 +1,38 @@
+from django.contrib import messages
+from django.test import TestCase
+from django.urls import reverse
+from clubs.enums import MomentType
+from clubs.models import Moment, Notification, User, Club, Meeting
+from clubs.tests.helpers import LogInTester, isUrlLegit
+from datetime import datetime, timedelta
+import pytz
+
+class MomentTestCase(TestCase, LogInTester):
+
+    fixtures = [
+        'clubs/tests/fixtures/default_user.json',
+        'clubs/tests/fixtures/default_book.json',
+        'clubs/tests/fixtures/default_club.json',
+        'clubs/tests/fixtures/other_users.json'
+        ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+        self.club = Club.objects.get(pk=1)
+        self.book = Club.objects.get(pk=1)
+
+    def test_moment_created_correctly(self):
+        self.client.login(username=self.user.username, password='Password123')
+        num = Moment.objects.all().count()
+        self.assertEqual(num,0)
+        new_moment = Moment.objects.create(body = 'New moment', type = MomentType.CUSTOM, user = self.user) 
+        new_moment.full_clean()
+        new_moment.save()
+        num_2 = Moment.objects.all().count()
+        self.assertEqual(num_2,1)
+        self.assertEqual(new_moment.type,MomentType.CUSTOM)
+        self.assertEqual(new_moment.type, 0)
+        saved_moment = Moment.objects.all()[0]
+        self.assertEqual(saved_moment, new_moment)
+
+    
